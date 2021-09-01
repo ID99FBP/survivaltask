6bd4b50b1766a002204f8b39dce70cd7de69c9aa
hawraa <alsafi99hawraa@gmail.com>
2021-09-01 17:04:54 +0300

    addd splash and icon

--- app/src/main/java/test/example/survivaltask/MainActivity.kt
+++ app/src/main/java/test/example/survivaltask/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Survivaltask)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(requireNotNull(binding.root))
         setup()
